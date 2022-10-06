72f449717b4451da3dc35b349ff09e6a44939ecc
Andres Moschini <andresmoschini@gmail.com>
2022-10-06 17:22:13 -0300

    test: add a sample test

---
+++ src/app/CustomerService.test.ts
@@ -0,0 +1,21 @@
+import { CustomerService } from './CustomerService';
+import { DbClient } from './DbClient';
+
+describe(CustomerService.name, () => {
+    describe("getAll", () => {
+        it("Should return an empty array when there are no items in DB", async () => {
+            // Arrange
+            const dbClientDouble: DbClient = {
+                put: async () => { /* do nothing */ },
+                scan: async () => ({ Items: [], Count: 0 })
+            };
+            const sut = new CustomerService(dbClientDouble);
+
+            // Act
+            const result = await sut.getAll();
+
+            // Assert
+            expect(result).toEqual([]);
+        });
+    });
+});
