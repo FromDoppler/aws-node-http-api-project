be91a8cfa5c095d645a9b64c5ceb5e99805bb7d1
Andres Moschini <andresmoschini@gmail.com>
2022-10-06 16:12:51 -0300

    Split code to optimize packages for each function

--- getCustomers.ts
+++ getCustomers.ts
@@ -1,28 +1,6 @@
 import { DynamoDB } from "aws-sdk";
 import { APIGatewayProxyHandler } from "aws-lambda";
 
-export const createCustomer: APIGatewayProxyHandler = async (event, context) => {
-    const body = JSON.parse(Buffer.from(event.body, 'base64').toString());
-    const dynamoDb = new DynamoDB.DocumentClient();
-    const putParams = {
-        TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
-        Item: {
-            primary_key: body.name,
-            email: body.email
-        }
-    };
-    await dynamoDb.put(putParams).promise();
-
-    return {
-        statusCode: 201,
-        body: JSON.stringify({
-            message: "Go Serverless v2.0! Your function executed successfully!",
-            context,
-            event,
-          })
-    };
-};
-
 export const getCustomers: APIGatewayProxyHandler = async (event, context) => {
     const scanParams = {
         TableName: process.env.DYNAMODB_CUSTOMER_TABLE
