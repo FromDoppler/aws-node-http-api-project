37a032702f2510249f0d652212796b92020b7245
Andres Moschini <andresmoschini@gmail.com>
2022-10-06 15:11:06 -0300

    Add base Serverless structure
    based on https://www.serverless.com/framework/docs/tutorial

---
+++ customer.js
@@ -0,0 +1,44 @@
+'use strict'
+const AWS = require('aws-sdk');
+
+module.exports.createCustomer = async (event) => {
+    const body = JSON.parse(Buffer.from(event.body, 'base64').toString());
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+    const putParams = {
+        TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
+        Item: {
+            primary_key: body.name,
+            email: body.email
+        }
+    };
+    await dynamoDb.put(putParams).promise();
+
+    return {
+        statusCode: 201
+    };
+};
+
+module.exports.getCustomers = async (event) => {
+    const scanParams = {
+        TableName: process.env.DYNAMODB_CUSTOMER_TABLE
+    };
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+    const result = await dynamoDb.scan(scanParams).promise();
+
+    if (result.Count === 0) {
+        return {
+            statusCode: 404
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            total: result.Count,
+            items: result.Items.map(customer => ({
+                name: customer.primary_key,
+                email: customer.email
+            }))
+        })
+    };
+};
