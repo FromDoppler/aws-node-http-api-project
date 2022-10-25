ff1b5b4b5040046e4c2e731f2f6db21bfdc6e595
Andres Moschini <andresmoschini@gmail.com>
2022-10-25 15:32:17 -0300

    feat: validate JWT token signature

--- src/createCustomer.ts
+++ src/createCustomer.ts
@@ -1,25 +1,29 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
-import { getCustomerService } from "./compositionRoot";
+import { getCustomerService, getJwtFilter } from "./compositionRoot";
 
 export const createCustomer: APIGatewayProxyHandler = async (
   event,
   context
 ) => {
+  // TODO: validate JWT Data (if isSU, accept any email, if not, only matching email)
   const customerService = getCustomerService();
+  const jwtFilter = getJwtFilter();
 
-  const body = JSON.parse(Buffer.from(event.body, "base64").toString());
-  await customerService.create({
-    name: body.name,
-    email: body.email,
-    lastVisit: null,
+  return jwtFilter.apply(event, async () => {
+    const body = JSON.parse(Buffer.from(event.body, "base64").toString());
+    await customerService.create({
+      name: body.name,
+      email: body.email,
+      lastVisit: null,
+    });
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: "Go Serverless v2.0! Your function executed successfully!",
+        context,
+        event,
+      }),
+    };
   });
-
-  return {
-    statusCode: 201,
-    body: JSON.stringify({
-      message: "Go Serverless v2.0! Your function executed successfully!",
-      context,
-      event,
-    }),
-  };
 };
--- src/getCustomer.ts
+++ src/getCustomer.ts
@@ -1,27 +1,28 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
-import { getCustomerService } from "./compositionRoot";
+import { getCustomerService, getJwtFilter } from "./compositionRoot";
 
 export const getCustomer: APIGatewayProxyHandler = async (event, context) => {
-  // TODO: validate JWT signature
   // TODO: validate JWT Data (if isSU, accept any email, if not, only matching email)
-  // TODO: search data in DB and return it
   const customerService = getCustomerService();
+  const jwtFilter = getJwtFilter();
 
-  const result = await customerService.get(event.pathParameters["email"]);
+  return jwtFilter.apply(event, async () => {
+    const result = await customerService.get(event.pathParameters["email"]);
 
-  if (!result) {
+    if (!result) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "Go Serverless v2.0! Your function executed successfully!",
+          context,
+          event,
+        }),
+      };
+    }
+
     return {
-      statusCode: 404,
-      body: JSON.stringify({
-        message: "Go Serverless v2.0! Your function executed successfully!",
-        context,
-        event,
-      }),
+      statusCode: 200,
+      body: JSON.stringify(result),
     };
-  }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(result),
-  };
+  });
 };
