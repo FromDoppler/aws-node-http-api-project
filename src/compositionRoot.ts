511a99687e449dc34afa940fa082275762886d56
Andres Moschini <andresmoschini@gmail.com>
2022-10-25 15:32:17 -0300

    feat: validate JWT token signature

--- src/compositionRoot.ts
+++ src/compositionRoot.ts
@@ -1,6 +1,8 @@
 import { DynamoDB } from "aws-sdk";
 import { CustomerService } from "./app/CustomerService";
 import { createDummyDynamoDbClient } from "./app/DummyDbClient";
+import { JwtFilter } from "./doppler-security/JwtFilter";
+import { JwtVerifier } from "./doppler-security/JwtVerifier";
 
 let _dbClientSingleton: DynamoDB.DocumentClient;
 function getDbClientSingleton(): DynamoDB.DocumentClient {
@@ -21,4 +23,17 @@ export function getCustomerService(): CustomerService {
       customerTableName: process.env.DYNAMODB_CUSTOMER_TABLE,
       dbClient: getDbClientSingleton(),
     }));
+}
+
+let _jwtVerifierSingleton: JwtVerifier;
+function getJwtVerifier(): JwtVerifier {
+  return (_jwtVerifierSingleton =
+    _jwtVerifierSingleton ||
+    new JwtVerifier({ publicKey: process.env.JWT_PUBLIC_KEY }));
+}
+
+let _jwtFilterSingleton: JwtFilter;
+export function getJwtFilter(): JwtFilter {
+  return (_jwtFilterSingleton =
+    _jwtFilterSingleton || new JwtFilter({ jwtVerifier: getJwtVerifier() }));
 }
--- src/getCustomers.ts
+++ src/getCustomers.ts
@@ -1,27 +1,31 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
-import { getCustomerService } from "./compositionRoot";
+import { getCustomerService, getJwtFilter } from "./compositionRoot";
 
 export const getCustomers: APIGatewayProxyHandler = async (event, context) => {
+  // TODO: validate JWT Data (if isSU, accept any email, if not, only matching email)
   const customerService = getCustomerService();
+  const jwtFilter = getJwtFilter();
 
-  const result = await customerService.getAll();
+  return jwtFilter.apply(event, async () => {
+    const result = await customerService.getAll();
 
-  if (result.length === 0) {
+    if (result.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "Go Serverless v2.0! Your function executed successfully!",
+          context,
+          event,
+        }),
+      };
+    }
+
     return {
-      statusCode: 404,
+      statusCode: 200,
       body: JSON.stringify({
-        message: "Go Serverless v2.0! Your function executed successfully!",
-        context,
-        event,
+        total: result.length,
+        items: result,
       }),
     };
-  }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      total: result.length,
-      items: result,
-    }),
-  };
+  });
 };
